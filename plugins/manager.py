3e16720ea084ae42ba71079a8f7f7180baeb2e30
YoungYukki <youngyukki@foxmail.com>
2024-07-15 23:27:46 +0800

    番剧管理功能

---
+++ plugins/manager.py
@@ -0,0 +1,68 @@
+import re
+
+
+class Manager:
+    def __init__(self) -> None:
+        self.read()
+        self.select_func()
+
+    def select_func(self):
+        func = input('1.选择\n2.添加\n3.删除\n执行功能:')
+        match func:
+            case '1': self.select_bangumi()
+            case '2': self.add_func()
+            case '3': self.remove_func()
+            case _: exit()
+
+    def add_func(self):
+        added_bangumi = input('添加的番剧:')
+        if added_bangumi:
+            self.add(added_bangumi)
+            self.add_func()
+        else:
+            self.select_func()
+
+    def remove_func(self):
+        removed_bangumi = input('删除的番剧:')
+        if removed_bangumi:
+            self.remove(removed_bangumi)
+            self.remove_func()
+        else:
+            self.select_func()
+
+    def select_bangumi(self):
+        number = 0
+        for bangumi in self.bangumi_list:
+            print(f'{number}.{bangumi[2:]}\t')
+            number += 1
+        selected_ID = input('选择番剧:')
+        if selected_ID:
+            self.bangumi = self.bangumi_list[int(selected_ID)][2:]
+        else:
+            exit()
+
+    def read(self):
+        with open('README.md', 'r', encoding='utf-8') as f:
+            text = f.read()
+        pattern = re.compile(r'\+\s.+')
+        self.bangumi_list = pattern.findall(text)
+        print(self.bangumi_list)
+
+    def add(self, bangumi: str):
+        bangumi = f'+ {bangumi}'
+        self.bangumi_list.append(bangumi)
+        self.commit()
+
+    def remove(self, bangumi: str):
+        bangumi = f'+ {bangumi}'
+        if not bangumi in self.bangumi_list:
+            print('不存在')
+            return None
+        self.bangumi_list.remove(bangumi)
+        self.commit()
+
+    def commit(self):
+        text = '# 目录\n'
+        text += '\n'.join(self.bangumi_list)
+        with open('README.md', 'w', encoding='utf-8') as f:
+            f.write(text)
